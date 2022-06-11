f746ca9b02fc2a21f8296ae36ee6dc15b013b760
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-06-11 16:28:19 +0100

    move info to dotenv

--- server/db.js
+++ server/db.js
@@ -1,11 +1,14 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user: "postgres",
-    password: "D4ntD1zz13!",
-    host: "localhost",
-    port: 5432,
-    database: "perntodo"
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DB
 })
 
 module.exports = pool
