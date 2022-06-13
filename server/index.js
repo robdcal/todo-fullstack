80ca364b40234b9e28ff03d4cb73af26117fcbda
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-06-13 09:48:21 +0100

    update CORS config

--- server/index.js
+++ server/index.js
@@ -7,23 +7,13 @@ const pool = require("./db");
 // app.use(cors())
 app.use(express.json());
 
-//Cors Configuration - Start
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-        res.header(
-            "Access-Control-Allow-Methods",
-            "POST, PUT, GET, DELETE"
-        )
-        return res.status(200).json({})
-    }
-    next()
-})
-//Cors Configuration - End
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 // Routes
 // create todo
